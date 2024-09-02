7e5a56aed4ac5a6b796a8e90606118139724ef8f
dhxuvv0003 <drjkhilosiya999@gmail.com>
2024-09-02 22:47:12 +0530

    testing

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Image src={Logo} alt="logo image" width={200} height={200} />
-      <h1>This is the main page after login</h1>
+      <h1>This is the main page after login.....</h1>
     </main>
   );
 }
